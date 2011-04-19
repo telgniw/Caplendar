4bc4c65517c3a9f10c64957501a87d07f87aea0b
Yi Huang <b97902044@ntu.edu.tw>
2011-04-19 12:13:01 +0800

    ADD files

---
+++ elaster/facebook/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>地圖行事曆</title>
+</head>
+<body>
+  <div id="fb-root">
+    <a href="http://date-map.app.elasterlabs.com.tw/" target="_blank">開啟地圖行事曆!</a>
+  </div>
+</body>
+</html>
