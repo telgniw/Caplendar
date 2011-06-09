0a062b559655338b4f7d12b0dfae00b70404738a
Yi Huang <b97902044@ntu.edu.tw>
2011-06-09 15:35:18 +0800

    MODIFY stable version

--- cat/base.py
+++ cat/base.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
-FB_API_KEY    = '8d955ca9f882c155282c7f3bbbda017c'
-FB_APP_ID     = '208577865834024'
-FB_APP_SECRET = 'ba90c5d51930b3bfc651d0a1d6819884'
+
 PERMS = 'user_events,create_event,rsvp_event,publish_stream'
 
+from common import *
 from db import *
 import facebook
 
--- cat/common.py
+++ cat/common.py
@@ -2,3 +2,6 @@
 # -*- coding: utf8 -*-
 
 APP_TITLE = '地圖行事曆'
+FB_API_KEY    = '8d955ca9f882c155282c7f3bbbda017c'
+FB_APP_ID     = '208577865834024'
+FB_APP_SECRET = 'ba90c5d51930b3bfc651d0a1d6819884'
